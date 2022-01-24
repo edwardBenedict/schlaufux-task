4e916fd1b9a546a2b24d74f29adf90d7a672499c
edwardBenedict <edwardbenedict92@gmail.com>
2022-01-24 22:45:50 +0100

    feat: add chooseOption function

--- CODE_SAMPLE_BASIC/main.js
+++ CODE_SAMPLE_BASIC/main.js
@@ -45,7 +45,7 @@ function getExerciseData() {
         console.log(response);
         questions = response.result.values;
         console.log("Questions", questions);
-        init();
+        questions && newQuestion();
       },
       function (response) {
         console.log("Error: " + response.result.error.message);
@@ -57,19 +57,34 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init() {
   let optionsContainer = document.querySelector("#options-wrapper");
-  //   console.log("init");
-  let options = questions[1][3].split(";");
   for (let i = 0; i < options.length; i++) {
     optionsContainer.innerHTML +=
       "<div class='unchosen option'><p class='text'>" +
       options[i] +
       "</p></div>";
   }
-  // ...
+}
+
+function newQuestion() {
+  let optionsContainer = document.querySelector("#options-wrapper");
+  if (questions) options = questions[1][3].split(";");
+
+  optionsContainer.innerHTML = "";
+  for (let i = 0; i < options.length; i++) {
+    optionsContainer.innerHTML +=
+      `<div class='unchosen option' id='${`option${i}`}' onclick='chooseOption(
+        ${i},${"questions[1]"},${`options[${i}]`}
+        )'=><p class='text'>` +
+      options[i] +
+      "</p></div>";
+  }
+  let questionWrapper = document.querySelector(".question");
+  questionWrapper.innerHTML = questions[1][2];
 }
 
 function toggleChoice(i) {
   states[i] = true;
+
   // ...
 }
 
@@ -86,4 +101,22 @@ function myEvaluation() {
       break;
     }
   }
+}
+
+function chooseOption(i, j, k) {
+  console.log("i", i, j, k);
+  for (let x = 0; x < options.length; x++) {
+    let option = document.querySelector(`#option${x}`);
+    if (x == i) {
+      option.classList.add("chosen");
+      option.classList.remove("unchosen");
+    } else {
+      option.classList.add("unchosen");
+      option.classList.remove("chosen");
+    }
+  }
+  // let selectedOption = document.querySelector(`#option${i}`);
+  // selectedOption.classList.add("chosen");
+  // console.log("selectedOption", selectedOption.innerHTML);
+  if (i == j[4]) console.log("point: ", j[5]);
 }
